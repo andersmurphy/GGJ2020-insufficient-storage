b180010560e888917918fef14cfad32f58e1b98a
andersmurphy <andersmurphy@gmail.com>
2020-01-31 16:15:44 +0000

    It moves!

--- src/clj_my_first_game/core.clj
+++ src/clj_my_first_game/core.clj
@@ -1,6 +1,7 @@
 (ns clj-my-first-game.core
   (:require [cljfx.api :as fx])
   (:import [javafx.scene.canvas Canvas]
+           [javafx.scene.input KeyCode KeyEvent]
            [javafx.scene.paint Color]))
 
 (def *state
@@ -19,8 +20,6 @@
                      :y      150
                      :height 50
                      :width  50}]}))
-
-(defmulti event-handler :event/type)
 
 (defn draw-entity [^Canvas canvas {:keys [color x y width height] :as m}]
   (doto (.getGraphicsContext2D canvas)
@@ -41,15 +40,23 @@
      :height  canvas-height
      :x       200
      :y       200
-     :scene   {:fx/type :scene
-               :root    {:fx/type  :h-box
-                         :children [{:fx/type :canvas
-                                     :height  canvas-height
-                                     :width   canvas-width
-                                     :draw    (partial draw-entities
-                                                       canvas-width
-                                                       canvas-height
-                                                       entities)}]}}}))
+     :scene   {:fx/type        :scene
+               :root           {:fx/type  :h-box
+                                :children [{:fx/type :canvas
+                                            :height  canvas-height
+                                            :width   canvas-width
+                                            :draw    (partial draw-entities
+                                                              canvas-width
+                                                              canvas-height
+                                                              entities)}]}
+               :on-key-pressed {:event/type :event/scene-key-press}}}))
+
+(defn event-handler  [e]
+  (case (:event/type e)
+    :event/scene-key-press
+    (let [key-code (.getCode ^KeyEvent (:fx/event e))]
+      (condp = key-code
+        KeyCode/ENTER (swap! *state update-in [:entities 0 :x] - 50)))))
 
 (def renderer
   (fx/create-renderer
